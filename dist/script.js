f65c8e4feebb770a0f47ac7d4bff442379c1e220
e-amstel <e_amstel@outlook.com>
2017-10-05 13:55:26 +0200

    second commit

--- dist/script.js
+++ dist/script.js
@@ -70,9 +70,93 @@
 "use strict";
 
 
-console.log([1, 2, 3].find(function (v) {
-  return v === 3;
-}));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); //het main script
+
+
+var _player = __webpack_require__(1);
+
+var _player2 = _interopRequireDefault(_player);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Spel = function () {
+    function Spel() {
+        _classCallCheck(this, Spel);
+
+        this.canvas = document.querySelector("#myCanvas");
+        this.context = this.canvas.getContext("2d");
+
+        this.player = new _player2.default(200, 200);
+        this.newPlayer();
+    }
+
+    _createClass(Spel, [{
+        key: "newPlayer",
+        value: function newPlayer() {
+            // console.log("de bal");  
+            this.player.draw(this.context);
+        }
+    }]);
+
+    return Spel;
+}();
+
+var spel = new Spel();
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+//de speler
+
+var Player = function () {
+    function Player(x, y) {
+        _classCallCheck(this, Player);
+
+        this.props = {
+            x: x,
+            y: y,
+            width: 10,
+            height: 10,
+            vel: { //speed
+                x: 10,
+                y: 10
+            },
+            color: this.randomColor()
+        };
+    }
+
+    _createClass(Player, [{
+        key: 'randomColor',
+        value: function randomColor() {
+            return 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+        }
+    }, {
+        key: 'draw',
+        value: function draw(context) {
+            context.fillStyle = this.props.color;
+            context.fillRect(this.props.x, this.props.y, this.props.width, this.props.height);
+            context.fill();
+        }
+    }]);
+
+    return Player;
+}();
+
+exports.default = Player;
 
 /***/ })
 /******/ ]);
---
+++ src/script.es6
@@ -0,0 +1,22 @@
+//het main script
+import Player from "./player.es6";
+
+class Spel {
+    constructor(){
+
+    this.canvas = document.querySelector("#myCanvas");
+    this.context = this.canvas.getContext("2d");
+
+    this.player = new Player(200,200);
+    this.newPlayer();
+    
+    }
+  
+  
+    newPlayer(){
+           // console.log("de bal");  
+            this.player.draw(this.context);
+        }    
+}    
+
+var spel = new Spel();
